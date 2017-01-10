0cbe5875f0dd329dd860148afc71469627d75644
Axel Mainguy <killerbossx@gmail.com>
2017-01-10 14:46:01 +0100

    LOL

---
+++ src/fr/axel/app/Poney - Copie (27).java
@@ -0,0 +1,26 @@
+package fr.axel.app;
+
+public class Poney {
+	public int age;
+	public String color;
+	public String name;
+
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getColor() {//MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud//MAJ Arnaud 
+		return color;
+	}
+	public void setColor(String color) { //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud 
+		this.color = color;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}	
+}
