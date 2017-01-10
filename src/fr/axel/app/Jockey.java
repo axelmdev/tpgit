d07fd531abd3a58bf2cbf76cbdff0203ff8d4e84
Axel Mainguy <killerbossx@gmail.com>
2017-01-10 12:17:11 +0100

    add Class Poney and Jockey java

---
+++ src/fr/axel/app/Jockey.java
@@ -0,0 +1,28 @@
+package fr.axel.app;
+
+public class Jockey {
+	public int age;
+	public String name;
+	public String lastname;
+	
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	
+}
---
+++ src/fr/axel/app/Poney.java
@@ -0,0 +1,27 @@
+package fr.axel.app;
+
+public class Poney {
+	public int age;
+	public String color;
+	public String name;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
