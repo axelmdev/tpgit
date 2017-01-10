341e9dca9010333914f37e252a1e0b4286e7ec0b
Axel Mainguy <killerbossx@gmail.com>
2017-01-10 14:07:50 +0100

    BLABLABLABA

--- src/fr/axel/app/Poney.java
+++ src/fr/axel/app/Poney.java
@@ -2,26 +2,26 @@ package fr.axel.app;
 
 public class Poney {
 	public int age;
-	public String color;
+	public String coloration;
 	public String name_poney;
 
 	public int getAge() {
 		return age;
 	}
-	public void setAge(int age) {
+	public void setAge(int age) {//MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud//MAJ Arnaud//MAJ Arnaud
 		this.age = age;
 	}
-	public String getColor() {
-		return color;
+	public String getColor() {//MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud//MAJ Arnaud 
+		return coloration;
 	}
-	public void setColor(String color) {
-		this.color = color;
+	public void setColor(String coloration) { //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud 
+		this.coloration = coloration;
 	}
 <<<<<<< HEAD
 	public String getName() {
 		return name_poney;
 =======
-	public String getName() { //MAJ Arnaud
+	public String getName() { //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud //MAJ Arnaud
 		return name;
 >>>>>>> 3ea516e04d9777e0d0923f1e6dab0a550a770781
 	}
