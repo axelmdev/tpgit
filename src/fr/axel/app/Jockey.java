ba4eb5dbcb596c7bc4dc6d5d4c1aab9035458407
Axel Mainguy <killerbossx@gmail.com>
2017-02-07 10:58:14 +0100

    Maj

--- src/fr/axel/app/Jockey.java
+++ src/fr/axel/app/Jockey.java
@@ -1,21 +1,21 @@
 package fr.axel.app;
 
 public class Jockey {
-	public int age;
-	public String name;
-	public String lastname;
 	
-	public int getAge() {
-		return age;
+	public int id;
+	public String firstname;
+	public String lastname;
+	public int weight;
+	
+	
+	public int getId() {
+		return id;
 	}
-	public void setAge(int age) {
-		this.age = age;
+	public String getFirstname() {
+		return firstname;
 	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
 	}
 	public String getLastname() {
 		return lastname;
