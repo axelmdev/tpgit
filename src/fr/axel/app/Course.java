f815e0ffe0e97c2331afe66ed5f530d98eb6b47e
Maurice Arnaud <arnaud.maurice.pro@gmail.com>
2017-02-07 10:58:12 +0100

    Ajout class Course

---
+++ src/fr/axel/app/Course.java
@@ -0,0 +1,21 @@
+package fr.axel.app;
+
+import java.sql.Date;
+
+public class Course {
+	
+	public int id;
+	public Date date;
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getId() {
+		return id;
+	}
+
+	
+}
