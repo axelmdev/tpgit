6c7687bb55b771a3015d4aaf247a1b0f5d9da864
Maurice Arnaud <arnaud.maurice.pro@gmail.com>
2017-02-07 11:04:26 +0100

    Ajout class Courir

---
+++ src/fr/axel/app/Courir.java
@@ -0,0 +1,33 @@
+package fr.axel.app;
+
+public class Courir {
+	public int rank;
+	private Jockey id;
+	private Course id;
+	private Poney id;
+	
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	public Jockey getId() {
+		return id;
+	}
+	public void setId(Jockey id) {
+		this.id = id;
+	}
+	public Course getId() {
+		return id;
+	}
+	public void setId(Course id) {
+		this.id = id;
+	}
+	public Poney getId() {
+		return id;
+	}
+	public void setId(Poney id) {
+		this.id = id;
+	}
+}
